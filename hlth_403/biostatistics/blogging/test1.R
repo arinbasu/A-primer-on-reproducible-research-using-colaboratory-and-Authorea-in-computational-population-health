f935af83873bf0656f38c32e78840abf71bbbd25
arinbasu <arin.basu@gmail.com>
2017-11-05 10:48:32 +1300

    made changes

---
+++ hlth_403/biostatistics/blogging/test1.R
@@ -0,0 +1,9 @@
+# install blogdown package
+# install.packages("blogdown")
+## update Hugo
+# blogdown::update_hugo()
+#
+# Create a new site using Hugo
+blogdown::new_site() # this failed because my project was not empty
+# Need to create a new project, empty folder and then start using it.
+# When you do that, you cannot even save a new file, you have to work from the console
